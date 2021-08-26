3b9de2037a4fc40d5fae0f551a14a92d0c3f46bf
Kunal <1901010010@krmangalam.ac.in>
2021-08-26 13:37:38 +0530

    created a simple stock price web app

---
+++ myapp.py
@@ -0,0 +1,22 @@
+import streamlit as st 
+import yfinance as yf  # yahoo finance API
+import pandas as pd 
+st.write("""
+# simple stock price app
+
+shown are the stock **closing price** and ***volume*** of google!
+"""
+)
+
+# define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+# get data on this ticker symbol
+tickerData = yf.Ticker(tickerSymbol)
+# get the historical prices for this ticker
+tickerDF = tickerData.history(period='1d', start='2015-5-31', end='2020-5-31')
+# open high low close volume dividends stocks splits
+
+
+st.line_chart(tickerDF.Close)
+st.line_chart(tickerDF.Volume)
