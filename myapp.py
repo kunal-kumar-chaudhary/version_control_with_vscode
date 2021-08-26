e8b24aa85a1f05d8f61744d4e5e8c29c9686a5e4
Kunal <1901010010@krmangalam.ac.in>
2021-08-26 13:51:18 +0530

    made a webapp

--- myapp.py
+++ myapp.py
@@ -8,14 +8,16 @@ shown are the stock **closing price** and ***volume*** of google!
 """
 )
 
+
 # define the ticker symbol
 tickerSymbol = 'GOOGL'
+
 
 # get data on this ticker symbol
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
 tickerDF = tickerData.history(period='1d', start='2015-5-31', end='2020-5-31')
-# open high low close volume dividends stocks splits
+# open high low Close Volume Dividends Stocks Splits
 
 
 st.line_chart(tickerDF.Close)
